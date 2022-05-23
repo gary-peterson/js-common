446d965f6a5afc38b1ffe17a579e02236b6d3e03
Bob Foo <bobfoo@bobbyfoo.com>
2022-05-22 22:43:35 -0500

    Added export for MatrixTool

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ run tests headless (console output)
 
 const
 	{MinimalMatrix} = require('./common-objects/minimal-matrix'),
+	{MatrixTool} = require('./common-objects/matrix-tool'),
 
 	{CustomException} = require('./common-tools/custom-exception'),
 	{Tools} = require('./common-tools/tools'),
@@ -25,6 +26,7 @@ JsExtensionLoader.load();
 
 //Exported Classes
 exports.MinimalMatrix = MinimalMatrix;
+exports.MatrixTool = MatrixTool;
 exports.CustomException = CustomException;
 exports.Tools = Tools;
 exports.MetaModel = MetaModel;
